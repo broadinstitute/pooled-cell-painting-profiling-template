1eb4f22704b9c753b97f815964d76f19b24bf310
gwaygenomics <gregory.way@gmail.com>
2020-07-22 17:26:55 -0400

    add bash script to perform the weld

---
+++ weld.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Perform the data pipeline welding procedure
+# This will link together a versioned processing pipeline (recipe) with versioned data
+#
+# Execution
+# To perform the weld, edit the configuration files in the config/ folder for the
+# specific experiment, and then execute:
+#
+# ./weld.sh
+
+preprocess_config="config/0.preprocess_sites_config.yaml"
+profiling_config="config/1.generate_profiles_config.yaml"
+
+#################################
+# Module 0 - Preprocess Sites
+#################################
+# Step 0 - Flag features to be removed later
+python pooled-cell-painting-recipe/0.preprocess-sites/0.prefilter-features.py --config_file $preprocess_config
+
+# Step 1 - Process the spot data (in situ sequencing readouts)
+python pooled-cell-painting-recipe/0.preprocess-sites/1.process-spots.py --config_file $preprocess_config
+
+# Step 2 - Process the cells data (cell painting readouts)
+python pooled-cell-painting-recipe/0.preprocess-sites/2.process-cells.py --config_file $preprocess_config
+
+# Step 3 - Visualize summary of preprocessing results
+python pooled-cell-painting-recipe/0.preprocess-sites/3.visualize-cell-summary.py --config_file $preprocess_config
+
+# Step 4 - Perform a series of QC checks and visualization
+python pooled-cell-painting-recipe/0.preprocess-sites/4.image-and-segmentation-qc.py --config_file $preprocess_config
+
+#################################
+# Module 1 - Generate Profiles
+#################################
+# Step 0 - Merge and provide cell assignments for all single cells
+python pooled-cell-painting-recipe/1.generate-profiles/0.merge-single-cells.py --config_file $profiling_config
+
+# Step 1 - Merge together single cell data to form aggregate profiles
+python pooled-cell-painting-recipe/1.generate-profiles/1.aggregate.py --config_file $profiling_config
+
+# Step 2 - Normalize profile data to enable valid comparisons
+python pooled-cell-painting-recipe/1.generate-profiles/2.normalize.py --config_file $profiling_config
+
+# Step 3 - Perform feature selection to isolate informative morphology features
+python pooled-cell-painting-recipe/1.generate-profiles/3.feature-select.py --config_file $profiling_config
