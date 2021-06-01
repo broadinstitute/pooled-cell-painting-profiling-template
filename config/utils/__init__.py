66fc31758a5c8ebec66bc02b70aa88c1c5866c24
gwaygenomics <gregory.way@gmail.com>
2021-06-01 18:14:59 -0400

    add function to parse the input yaml options

--- config/utils/__init__.py
+++ config/utils/__init__.py
@@ -3,4 +3,5 @@ import sys
 sys.path.append("config")
 
 from utils.arg_utils import parse_command_args
-from utils.config_utils import get_plates, process_configuration
+from utils.config_utils import get_batches, process_configuration
+from utils.data_split_utils import get_split_aware_site_info
